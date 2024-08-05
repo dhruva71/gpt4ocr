ca1032e28ddc527d82b9e8424a0a97f76d9076e0
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-08-05 21:48:20 +0530

    Added a new function to encapsulate what was in main.rs to simplify usage

--- src/gpt4o.rs
+++ src/gpt4o.rs
@@ -1,10 +1,10 @@
 use std::env;
-
+use std::error::Error;
 use openai_api_rs::v1::api::OpenAIClient;
 use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
 use openai_api_rs::v1::common::GPT4_O_MINI;
 
-use crate::{errors, file_handlers};
+use crate::{errors, file_handlers, images_from_pdf};
 
 pub fn create_openai_client(api_key: &str) -> Result<OpenAIClient, errors::APIKeyError> {
     let openai_api_key: String;
@@ -112,4 +112,68 @@ mod tests {
         let client = create_openai_client(api_key);
         assert!(!client.is_ok()); // should return an error since the API key is empty
     }
+}
+
+/// Automatically extract text from a PDF file, and generate JSON from it. It then saves the JSON to a file in the same directory as the PDF file.
+///
+/// # Arguments
+///
+/// * `target_document` - a string slice that holds the path to the PDF file.
+/// * `openai_api_key` - a string slice that holds the OpenAI API key.
+/// * `extraction_prompt` - a string slice that holds the prompt for the AI model.
+/// * `save_json` - a boolean that determines whether to save the JSON to a file.
+///
+/// # Returns
+///
+/// Result<Vec<String>, Box<dyn Err>> - a vector of strings that holds the generated JSONs, or an error.
+pub async fn extract_json_from_pdf(target_document: &str, openai_api_key: &String, extraction_prompt: &str, save_json: bool) -> Result<Vec<String>, Box<dyn Error>> {
+    // Step 1: Extract images from the PDF file
+    let image_paths = images_from_pdf::extract_images_from_pdf(target_document).unwrap();
+
+    // Step 2: Run OCR on the extracted images
+    let prompt: String;
+    if extraction_prompt.is_empty() {
+        println!("No prompt provided. Using default prompt.");
+        prompt = "Extract text from the images, and generate JSON from it. Respond only with the json.".to_string();
+    } else {
+        prompt = extraction_prompt.to_string();
+    }
+
+    let mut generated_jsons: Vec<String> = Vec::new();
+
+    for image_path in image_paths {
+        let response_json = run_ocr_on_image(create_openai_client(&openai_api_key).unwrap(), image_path.as_str(), prompt.as_str()).await;
+        match response_json {
+            Ok(json) => {
+                println!("{}", json);
+                let json_path = image_path + ".json";
+                dbg!(&json_path);
+
+                generated_jsons.push(json.clone());
+
+                // if save_json is false, skip saving the JSON to a file
+                if !save_json {
+                    continue;
+                }
+
+                let json_save = file_handlers::save_json_to_file(json.as_str(), json_path.as_str()).await;
+                match json_save {
+                    Ok(_) => {
+                        println!("JSON saved to: {}", json_path);
+                    }
+                    Err(e) => {
+                        dbg!(&e);
+                        return Err(e);
+                    }
+                }
+            }
+            Err(e) => {
+                dbg!(&e);
+                return Err(e);
+            }
+        }
+    }
+
+    // return the generated JSONs
+    Ok(generated_jsons)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use std::env;
 
 use dotenvy::dotenv;
 
-use gpt4ocr::{file_handlers, images_from_pdf};
-
 use gpt4ocr::gpt4o;
-
 
 #[tokio::main]
 async fn main() {
@@ -15,31 +12,13 @@ async fn main() {
     let target_document = "pdfs/Resume.pdf";
     let openai_api_key = env::var("OPENAI_API_KEY").expect("failed to load OpenAI API key");
 
-    // Step 1: Extract images from the PDF file
-    let image_paths = images_from_pdf::extract_images_from_pdf(target_document).unwrap();
-
-    // Step 2: Run OCR on the extracted images
-    let prompt: &str = "Extract text from the images, and generate JSON from it. Respond only with the json.";
-    for image_path in image_paths {
-        let response_json = gpt4o::run_ocr_on_image(gpt4o::create_openai_client(&openai_api_key).unwrap(), image_path.as_str(), prompt).await;
-        match response_json {
-            Ok(json) => {
-                println!("{}", json);
-                let json_path = image_path + ".json";
-                dbg!(&json_path);
-                let json_save = file_handlers::save_json_to_file(json.as_str(), json_path.as_str()).await;
-                match json_save {
-                    Ok(_) => {
-                        println!("JSON saved to: {}", json_path);
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                    }
-                }
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
+    let generated_jsons = gpt4o::extract_json_from_pdf(target_document, &openai_api_key, "", false).await;
+    match generated_jsons {
+        Ok(jsons) => {
+            println!("JSONs generated: {:?}", jsons);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
         }
     }
 }
